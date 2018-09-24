faf9129a041ccb42d5a575cde693619b5a9ed5ba
Your Name <furqanpersonal@gmail.com>
2018-09-24 15:11:26 +0500

    ngIf Conditions

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server created!';
   serverName = '';
   userName = '';
+  serverCreated=false;
   resetUserNameFlag = true;
   constructor() { 
     setTimeout(()=>{
@@ -31,6 +32,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(){
+    this.serverCreated = true;
+
     this.serverCreationStatus = 'Server was created & Name is '+this.serverName;
   }
 
